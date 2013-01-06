676504483017ec4821b6e2403b2b535f508fa339
mutantmonkey <mutantmonkey@mutantmonkey.in>
2013-01-06 17:45:48 +0100

    add pairing support

--- remote.py
+++ remote.py
@@ -13,14 +13,31 @@ class YouTubeRemote(object):
     gsessionid = ""
     seq = 0
 
-    def __init__(self, remote_id, remote_app, remote_name, token):
-        self.token = token
+    def __init__(self, remote_id, remote_app, remote_name):
+        self.remote_id = remote_id
+        self.remote_app = remote_app
+        self.remote_name = remote_name
 
+    def pair(self, pairing_code):
+        pairing_code.replace(" ", "")
+
+        r = requests.post("https://www.youtube.com/api/lounge/pairing/get_screen",
+                data={'pairing_code': pairing_code}, headers={
+                    'User-Agent': "Apache-HttpClient/UNAVAILABLE (java 1.4)",
+                })
+        data = json.loads(r.text)
+        self.load_token(data)
+        return data
+
+    def load_token(self, data):
+        self.token = data['screen']['loungeToken']
+
+    def connect(self):
         r = requests.get('http://www.youtube.com/api/lounge/bc/test?VER=8&TYPE=xmlhttp')
 
         data = self._send('http://www.youtube.com/api/lounge/bc/bind?RID=1&VER=8&CVER=1&id={remote_id}&device=REMOTE_CONTROL&app={remote_app}&name={remote_name}'.\
-                format(remote_id=remote_id, remote_app=remote_app,
-                    remote_name=urllib.parse.quote(remote_name)))
+                format(remote_id=self.remote_id, remote_app=self.remote_app,
+                    remote_name=urllib.parse.quote(self.remote_name)))
         self.sid = data[0][1][1]
         self.gsessionid = data[1][1][1]
 
@@ -32,7 +49,6 @@ class YouTubeRemote(object):
         data = "\n".join(r.text.splitlines()[1:])
         data = json.loads(data)
         return data
-
 
     def do(self, data):
         apidata = {
@@ -76,17 +92,28 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(
             description="Command-line YouTube Leanback remote")
-    parser.add_argument('--play', help="Play a video immediately")
-    parser.add_argument('--queue', nargs='*',
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('--play', help="Play a video immediately")
+    group.add_argument('--queue', nargs='*',
             help="Add a video to the queue")
-    parser.add_argument('--pause', action="store_true",
+    group.add_argument('--pause', action="store_true",
             help="Pause the current video")
-    parser.add_argument('--unpause', action="store_true",
+    group.add_argument('--unpause', action="store_true",
             help="Unpause the current video")
     args = parser.parse_args()
 
     remote = YouTubeRemote(config.REMOTE_ID, config.REMOTE_APP,
-            config.REMOTE_NAME, config.TOKEN)
+            config.REMOTE_NAME)
+
+    try:
+        data = json.load(open('screen.json'))
+        remote.load_token(data)
+    except IOError:
+        data = remote.pair(input("Pairing code: "))
+        with open('screen.json', 'w') as f:
+            f.write(json.dumps(data))
+
+    remote.connect()
 
     if args.play:
         remote.set(args.play)
